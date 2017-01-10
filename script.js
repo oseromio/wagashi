d92895997f44e94f70f357e6280e87ec230962b5
mioishiguro <mmmochaio@gmail.com>
2017-01-10 18:33:54 +0900

    ページ内移動の修正

--- script.js
+++ script.js
@@ -2,32 +2,22 @@ $(function(){
   $('.carousel').carousel({
     interval: 2500
   });
-
-         $('.fa-heart-o').click(function(){
-           if($(this).hasClass('fa-heart-o')){
-             $(this).removeClass('fa-heart-o');
-             $(this).addClass('fa-heart');
-           } else{
-             $(this).addClass('fa-heart-o')
-           }
-         });
-
+   $('.fa-heart-o').click(function(){
+     if($(this).hasClass('fa-heart-o')){
+       $(this).removeClass('fa-heart-o');
+       $(this).addClass('fa-heart');
+     } else{
+       $(this).addClass('fa-heart-o')
+     }
+   });
   //headerのスクロール色表示
   $(window).scroll(function() {
       if ($(window).scrollTop() >= 430) {
-   $(".header").css("background-color" , "rgba(231, 228, 211, 0.9)");
- } else {
-   $(".header").css("background-color","rgba(255, 255, 255, 0)");
- }
-});
-
-// $(window).scroll(function() {
-//     if ($(window).scrollTop() >= 430) {
-//  $("#ham-menu").css("background-color" , "rgba(255,255,255,0.9)");
-// } else {
-//  $("#ham-menu").css("background-color","rgba(255, 255, 255, 0.9)");
-// }
-// });
+          $(".header-inner").css("background-color" , "rgba(231, 228, 211, 0.9)");
+      } else {
+          $(".header-inner").css("background-color","rgba(255, 255, 255, 0)");
+      }
+  });
 
 // トップへ戻るボタン
   $('#top-btn').click(function(){
@@ -35,11 +25,10 @@ $(function(){
       'scrollTop': 0
     }, 500);
   });
-
 // ページ内リンク
 $('.cadegory-btn a').click(function(){
   var id = $(this).attr('href');
-    var position = $(id).offset().top;
+    var position = $(id).offset().top -50;
     $('html, body').animate({
       'scrollTop': position
     }, 700)
@@ -47,21 +36,27 @@ $('.cadegory-btn a').click(function(){
 
 $('.disappear a').click(function(){
   var id = $(this).attr('href');
-    var position = $(id).offset().top;
+    var position = $(id).offset().top -50;
     $('html, body').animate({
       'scrollTop': position
+    }, 700)
+});
+
+$('#ham-menu li').click(function(){
+  var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop()': position
     }, 700)
 });
 
 $('#ham-menu a').click(function(){
   var id = $(this).attr('href');
-    var position = $(id).offset().top;
+    var position = $(id).offset().top -50;
     $('html, body').animate({
       'scrollTop': position
     }, 700)
 });
-
-
 
  $('.fa-bars').click(function(){
    if($(this).hasClass('fa-bars')){
